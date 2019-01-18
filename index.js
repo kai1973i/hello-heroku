b1d999519f5abe91a8ccdf3c1724f259333cc0fc
kai1973i <kai1973i@googlemail.com>
2019-01-18 22:05:42 +0100

    change console log
    Signed-off-by: kai1973i <kai1973i@googlemail.com>

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ const server = http.createServer((req, res)=>{
 })
 
 server.listen(PORT, () => {
-   console.log('Server running on ${port}');  
+   console.log('Server running on ${port}/');  
 });
